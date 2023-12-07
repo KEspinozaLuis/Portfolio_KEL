ab0fe1f439af9181979a232466aead7f4771ab46
Keny Espinoza Luis <kespinozaluis@gmail.com>
2023-12-07 15:03:24 -0500

    Update banner principal, menú de navegación

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,17 +1,18 @@
 import { BsFillEnvelopeFill, BsLinkedin, BsGithub } from "react-icons/bs";
 
-
-
 const Banner = () => {
     return (
-        <section className="wrapper banner">
+        <section className="wrapper banner" id="banner">
+            <div className="containerImg">
+                <img src="./img/avatar.svg" alt="foto perfil" />
+            </div>
             <div className="containerInfo">
                 <div className="content info">
                     <h1 className="name">Keny Espinoza</h1>
                     <span className="role">Front end Developer</span>
                 </div>
                 <div className="content description">
-                    <p>Fuerte pasión por la creación de experiencias de usuario asombrosas, donde combino la creatividad del diseño con la lógica del código.</p>
+                    <p>Fuerte pasión por la creación de experiencias de usuario asombrosas combinando la creatividad del diseño con la lógica del código.</p>
                 </div>
                 <div className="content buttons">
                     <button className="btn btnPrimary">Contacto</button>
@@ -22,9 +23,6 @@ const Banner = () => {
                     <a href=""><BsLinkedin /></a>
                     <a href=""><BsGithub /></a>
                 </div>
-            </div>
-            <div className="containerImg">
-                <img src="./img/avatar.svg" alt="foto perfil" />
             </div>
         </section>
     )
