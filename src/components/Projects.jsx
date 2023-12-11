e4d76d40c9c00171ace52302bf66f63f3e27e34c
Keny Espinoza Luis <kespinozaluis@gmail.com>
2023-12-11 16:15:06 -0500

    Ajuste mixins, section projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,50 @@
 
 const Projects = () => {
     return (
-        <section className="wrapper section">
+        <section className="wrapper section projects" id="projects">
             <h1 className="title">Proyectos</h1>
+            <div className="containerProjects">
+                <div className="project">
+                    <img src="./projects/puraVidaViajes.png" alt="Pura Vida Viajes Premium" />
+                    <div className="decriptionProject">
+                        <h2>Viajes Pura Vida</h2>
+                        <div className="containerBtns">
+                            <button className="btn btnPrimary">Demo</button>
+                            <button className="btn btnPrimary">Github</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="project">
+                    <img src="./projects/puraVidaViajes.png" alt="" />
+                    <div className="decriptionProject">
+                        <h2>PokeApi</h2>
+                        <div className="containerBtns">
+                            <button className="btn btnPrimary">Demo</button>
+                            <button className="btn btnPrimary">Github</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="project">
+                    <img src="./projects/puraVidaViajes.png" alt="" />
+                    <div className="decriptionProject">
+                        <h2>Dogs Api</h2>
+                        <div className="containerBtns">
+                            <button className="btn btnPrimary">Demo</button>
+                            <button className="btn btnPrimary">Github</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="project">
+                    <img src="./projects/puraVidaViajes.png" alt="Pura Vida Viajes Premium" />
+                    <div className="decriptionProject">
+                        <h2>Viajes Pura Vida</h2>
+                        <div className="containerBtns">
+                            <button className="btn btnPrimary">Demo</button>
+                            <button className="btn btnPrimary">Github</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
